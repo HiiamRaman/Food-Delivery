4556c78c6836b87fb14061761fe3a874065b3297
Raman Singh <ramansinghchauhan787@gmail.com>
2025-12-23 16:12:33 +0545

    first commit

---
+++ frontend/Food-Delivery/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar/Navbar.jsx';
+function App() {
+  return (
+    <div>
+      <h1>Hello From Raman</h1>
+ <div>
+
+      <Navbar />
+ </div>
+
+
+    </div>
+  )
+}
+
+export default App
