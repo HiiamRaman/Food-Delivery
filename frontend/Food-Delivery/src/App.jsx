82883679561aed087ab3157970253a0e3b0eea24
Raman Singh <ramansinghchauhan787@gmail.com>
2025-12-25 22:09:02 +0545

    updated components and their respective  css

--- frontend/Food-Delivery/src/App.jsx
+++ frontend/Food-Delivery/src/App.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx';
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home/Home.jsx';
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx';
 function App() {
   return (
-    <div>
+    
      
  <div className='app'>
 
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order'element={<PlaceOrder/>} />
+
+      </Routes>
  </div>
 
 
-    </div>
+   
   )
 }
 
--- frontend/Food-Delivery/src/components/Navbar/Navbar.jsx
+++ frontend/Food-Delivery/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../../assets/assets'
 import "./Navbar.css"
 function Navbar() {
+  const [menu,setMenu] = useState("Home")
+ 
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className='logo'/>
       <ul className='navbar-menu'>
-        <li>Home</li>
-        <li>Menu</li>
-        <li>MobileApp</li> 
-        <li>ContactUs</li>
+        <li onClick={()=>{setMenu("Home")}} className={menu==="Home"?"active":""}>Home</li>
+        <li onClick={()=>{setMenu("Menu")}} className={menu==="Menu"?"active":""}>Menu</li>
+        <li onClick={()=>{setMenu("MobileApp")}} className={menu==="MobileApp"?"active":""}>MobileApp</li> 
+        <li onClick={()=>{setMenu("ContactUs")}} className={menu==="ContactUs"?"active":""}>ContactUs</li>
       </ul>
       <div className='navbar-right'>
           <img src={assets.search_icon} alt="" />
-          <div className="navbar-seach-icon">
+          <div className="navbar-search-icon">
             <img src={assets.basket_icon} alt="" />
+            <div className='dot'></div>
           </div>
       </div>
       <button>sign in</button>
