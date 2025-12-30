7ecab81f7fdd4600ccf944872115a2f85c81d1d8
Raman Singh <ramansinghchauhan787@gmail.com>
2025-12-30 14:02:02 +0545

    added apiError.js

---
+++ backend/src/utils/apiError.js
@@ -0,0 +1,21 @@
+export class ApiError extends Error {
+  constructor(
+    statusCode,
+    data = null,
+    message = "something went wrong !",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
