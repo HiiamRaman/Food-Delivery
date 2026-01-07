2108ce479fb064556a9c8cda6faa61f4e25d9d42
Raman Singh <ramansinghchauhan787@gmail.com>
2026-01-07 14:10:04 +0545

    updated list.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 function List() {
+  const [list, setList] = useState([]);
+  const url = "http://localhost:3000";
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/v1/allfoods`);
+    console.log(response.data);
+    if (response.data.success) {
+      setList(response.data.data.foods);
+    } else {
+      toast.error("Erorr!!");
+    }
+  };
+  useEffect(() => {
+    fetchList();
+  }, []);
   return (
-    <div>
-      <h1>List</h1>
+    <div className="list add flex-col">
+      <p> All Food List</p>
+      <div className="list-table">
+        <div className="list-table-format-title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          console.log(item.image);
+          return (
+            <div key={index} className="list-table-format">
+              <img
+                src={item.image}
+                alt={item.name}
+                onError={(e) => {
+                  e.target.src = "/fallback.png"; //this is image handler when image break this will help
+                }}
+              />
+              <p>{item.name}</p>
+              <p>${item.price}</p>
+              <p>{item.category}</p>
+              <p>X</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
