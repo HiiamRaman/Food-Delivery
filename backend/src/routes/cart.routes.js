e19f26df5bb6bc99272322b34b72bd91af1f082b
Raman Singh <ramansinghchauhan787@gmail.com>
2026-01-21 22:31:58 +0545

    added cart routes

--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { addToCart,removeCart } from "../controllers/cart.controller.js";
+import { addToCart,applyCoupon,clearCart,getCart,removeCart } from "../controllers/cart.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 const router = Router()
 
+router.route('/').get(verifyJWT,getCart);
+router.route('/').delete(verifyJWT,clearCart)
 router.route('/add').post(verifyJWT,addToCart);
 router.route('/remove/:foodId').delete(verifyJWT,removeCart);
-
+router.route('/coupon/apply').post(verifyJWT,applyCoupon);
+router.route('/coupon/remove').delete(verifyJWT,removeCart)
 export default router
