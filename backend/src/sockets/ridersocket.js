80f0e41affb231c8550422607487f62dc6072b60
Raman Singh <ramansinghchauhan787@gmail.com>
2026-04-19 10:44:57 +0545

    updated socket

--- backend/src/sockets/ridersocket.js
+++ backend/src/sockets/ridersocket.js
@@ -1,27 +1,29 @@
 export const riderSocket = (io) => {
+
   io.on("connection", (socket) => {
-    console.log("Rider Connected ", socket.id);
-    // 1. Rider joins their room
-    socket.on("join_Rider", (riderId) => {
-      socket.join(riderId);
-      console.log(`🚴 Rider joined room: ${riderId}`);
+    console.log("Rider Connected", socket.id);
+
+    socket.on("start_demo_route", (route) => {
+      // route = array of lat/lng points from frontend or backend
+      let index = 0;
+
+      const interval = setInterval(() => {
+        if (index >= route.length) {
+          clearInterval(interval);
+          return;
+        }
+
+        const currentPosition = route[index];
+
+        io.emit("rider_location_update", currentPosition);
+
+        index++;
+      }, 4000); // smooth movement speed
     });
-    // 2. Rider sends live location
-    socket.on("rider_location", (data) => {
-      //data = {riderId,lat,lng}
-      console.log("Location Received", data);
-      // Broadcast to all clients (customers/admin)
-      io.emit("rider_location_update", data);
-    });
-    // 3. Order assigned to rider
-    socket.on("assign_Order", (data) => {
-      // data = { riderId, order }
-      console.log("Order assigned", data);
-      io.to(data.riderId).emit("new_order", data.order);
-    });
-    //disconnect
+
     socket.on("disconnect", () => {
       console.log("Rider Disconnected", socket.id);
     });
   });
+
 };
