dcdcf3d3bfb7b66bcbc00e4bf8253e2ad66231e6
Raman Singh <ramansinghchauhan787@gmail.com>
2026-04-19 14:09:31 +0545

    update map

--- frontend/Food-Delivery/src/socket/Map.jsx
+++ frontend/Food-Delivery/src/socket/Map.jsx
@@ -1,43 +1,3 @@
-// import { MapContainer, TileLayer, Marker } from "react-leaflet";
-// import { useEffect, useState } from "react";
-// import { io } from "socket.io-client";
-// import "leaflet/dist/leaflet.css";
-
-// // connect to backend socket
-// const socket = io("http://localhost:3000");
-
-// function LiveMap() {
-//   const [position, setPosition] = useState([27.7172, 85.3240]); // default Kathmandu
-
-//   useEffect(() => {
-//     // listen for backend updates
-//     socket.on("rider_location_update", (data) => {
-//       console.log("Received:", data);
-
-//       setPosition([data.lat, data.lng]);
-//     });
-
-//     return () => {
-//       socket.off("rider_location_update");
-//     };
-//   }, []);
-
-//   return (
-//     <MapContainer center={position} zoom={14} style={{ height: "400px", width: "100%" }}>
-
-//       <TileLayer
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//       />
-
-//       {/* This marker moves */}
-//       <Marker position={position} />
-
-//     </MapContainer>
-//   );
-// }
-
-// export default LiveMap;
-
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 import { Polyline } from "react-leaflet";
 import { useEffect, useState } from "react";
@@ -77,22 +37,36 @@ function LiveMap() {
     };
   }, []);
 
-  const startDemo = () => {
-    const path = [
-      { lat: 27.7172, lng: 85.324 },
-      { lat: 27.721, lng: 85.325 },
-      { lat: 27.725, lng: 85.33 },
-      { lat: 27.727, lng: 85.336 },
-      { lat: 27.729, lng: 85.339 },
+  // const startDemo = () => {
+  //   const path = [
+  //     { lat: 27.7172, lng: 85.324 },
+  //     { lat: 27.721, lng: 85.325 },
+  //     { lat: 27.725, lng: 85.33 },
+  //     { lat: 27.727, lng: 85.336 },
+  //     { lat: 27.729, lng: 85.339 },
+  //   ];
+  //   setRoute(path);
+  //   socket.emit("start_demo_route", path);
+  // };
+
+  const startOrderTracking = () => {
+    const routeData = [
+     { lat: 27.7172, lng: 85.324 },
+    { lat: 27.721, lng: 85.325 },
+    { lat: 27.725, lng: 85.33 },
+    { lat: 27.727, lng: 85.336 },
+    { lat: 27.729, lng: 85.339 },
     ];
-    setRoute(path);
-    socket.emit("start_demo_route", path);
+    setRoute(routeData);
+    socket.emit("start_order_tracking", {
+      route: routeData,
+    });
   };
 
   return (
     <div style={{ padding: "20px" }}>
       <button
-        onClick={startDemo}
+        onClick={startOrderTracking}
         style={{ marginBottom: "10px", padding: "10px", cursor: "pointer" }}
       >
         🚀 Start Delivery Demo
@@ -113,7 +87,7 @@ function LiveMap() {
 
         {/* Restaurant (Start) */}
         <Marker position={startPoint} icon={resturantIcon} />
-       <Marker position={endPoint}  icon={homeIcon}/>
+        <Marker position={endPoint} icon={homeIcon} />
         <Polyline
           positions={route.map((point) => [point.lat, point.lng])}
           color="blue"
