ac9c3dfaa298a2197bd18148b16139eb9096a093
Raman Singh <ramansinghchauhan787@gmail.com>
2026-05-02 16:06:07 +0545

    added admin-disptach route

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { createOrder,getAllOrders} from "../controllers/order.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { orderWorkflowController } from "../controllers/order.controller.js";
+import { orderWorkflowController,adminDispatchOrder } from "../controllers/order.controller.js";
 const router = express.Router();
 
 router.post("/create", verifyJWT, createOrder);
 router.get("/allOrders",getAllOrders)
 router.post("/:orderId/workflow", orderWorkflowController);
-
+router.post("/:orderId/admin-dispatch", adminDispatchOrder)
 export default router;
